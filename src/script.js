516bee0b4a68f6480c0c6b91dc53f4d8160e870f
camyla <camylavsp@gmail.com>
2025-05-20 16:37:00 -0300

    terceiro

--- src/script.js
+++ src/script.js
@@ -42,3 +42,19 @@ class MobileNavbar {
     ".nav-list li",
   );
   mobileNavbar.init();
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+
+}, 5000)
+
+function nextImage(){
+  count++;
+  if(count>4){
+    count = 1;
+  }
+  document.getElementById("radio"+count).checked = true;
+}
+
