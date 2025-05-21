37bc5b1a9e21c67a4c612d47bee8790ef7b57981
camyla <camylavsp@gmail.com>
2025-05-21 08:50:10 -0300

    quarto

--- src/script.js
+++ src/script.js
@@ -43,18 +43,18 @@ class MobileNavbar {
   );
   mobileNavbar.init();
 
-let count = 1;
-document.getElementById("radio1").checked = true;
-
-setInterval(function(){
-
-}, 5000)
-
-function nextImage(){
-  count++;
-  if(count>4){
-    count = 1;
+  let count = 1;
+  document.getElementById("radio1").checked = true;
+  
+  setInterval(function () {
+      nextImage();
+  }, 5000); // Troca de imagem a cada 5 segundos
+  
+  function nextImage() {
+      count++;
+      if (count > 4) {
+          count = 1;
+      }
+      document.getElementById("radio" + count).checked = true;
   }
-  document.getElementById("radio"+count).checked = true;
-}
 
